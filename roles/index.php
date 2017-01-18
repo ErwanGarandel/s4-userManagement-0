8385d4e266f4157f7b996755ed4bb8d4da034b7a
ErwanGarandel <erwangarandel@gmail.com>
2017-01-18 15:57:17 +0100

    DernierCommit

--- roles/index.php
+++ roles/index.php
@@ -23,25 +23,44 @@ $retour_name=$bdd->query('SELECT * from ROLE');
 
 //$retour_name->execute();
 //$donnees = $retour_name ->fetch();
+echo "<table><thead><th>Nom :</th><th>NBUSER :</th></thead><tr><td>";
 while($donnees = $retour_name->fetch())
 {
-echo "<table><tr><td>Nom : ";
 echo "";
-echo $donnees['name']."</td><tr>";
-echo "</table><br/>";
+echo $donnees['name'];
+echo "<br/>";
 }
 $retour_name->closeCursor(); 
-
+echo "</td></tr></table>";
  #########################
 
-$retour_nb_user=$bdd->query('SELECT count(*) as TOTAL from ROLE');
-$retour_nb_user->execute();
-while($nb_user=$retour_nb_user->fetch()){
-	echo "<table><tr><td>NBUSER : ";
-	echo $nb_user['TOTAL']."</td></tr></table>";
+$retour_nb_role=$bdd->query('SELECT count(*) as TOTAL_ROLE from ROLE');
+$retour_nb_role->execute();
+while($nb_role=$retour_nb_role->fetch()){
+	echo "<table><tr><td>NbRole : ";
+	echo $nb_role['TOTAL_ROLE']."</td></tr></table>";
+	echo "<br/>";
 }
 
-echo "$nb_user";
+##########################
+
+$nb_user=$bdd->query('SELECT count(*) as TOTAL_USER from USER');
+$nb_user->execute();
+while($nb_userT=$nb_user->fetch()){
+	echo "<table><tr><td>NbRole : ";
+	echo $nb_userT['TOTAL_USER']."</td></tr></table>";
+	echo "<br/>";
+}
+
+############################
+$nb_admin=$bdd->query('SELECT count(*) as TOTAL_ADMIN , name from USER us join ROLE ro on us.id=ro.id where name="admin"');
+$nb_admin->execute();
+while($nb_adminA=$nb_admin->fetch()){
+	echo "<table><tr><td>NbRole : ";
+	echo $nb_adminA['TOTAL_ADMIN'];
+	echo "&nbsp;&nbsp;&nbsp		Name : ".$nb_adminA['name']."</td></tr></table>";
+	echo "<br/>";
+}
 ?>
 </body>
 </html>
